096f2e708491fdabcc14a57f312790badfb098b2
Nicolas Fan <nicfan@gmail.com>
2018-04-07 12:46:57 -0400

    Created initial routes to landing pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/mealplanner', to: "pages#mealplanner", as: 'mealplanner'
+  get '/review', to: "pages#review", as: 'review'
+  get '/statistics', to: "pages#statistics", as: 'statistics'
 end
